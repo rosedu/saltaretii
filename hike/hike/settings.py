bf88fbc4f8900aaca0b6b1146069c254363317ba
Andrei Petre <p31andrei@gmail.com>
2013-10-19 22:46:24 +0300

    Add /static prefix for index.html files. #9

--- hike/hike/settings.py
+++ hike/hike/settings.py
@@ -5,7 +5,7 @@ TEMPLATE_DEBUG = DEBUG
 
 # Because actually hard-coding absolute paths into your code would be bad...
 import os
-PROJECT_DIR = os.path.dirname(__file__)
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 ADMINS = (
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '',
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
