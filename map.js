728f1655ca4740db33dae024ded1db7f42b9da4b
Andrei Popescu <popescu.andrei1991@gmail.com>
2013-10-19 16:22:07 +0300

    set map to start from geolocation

--- map.js
+++ map.js
@@ -8,5 +8,10 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 }
-
+if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(function (position) {
+         initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+         map.setCenter(initialLocation);
+     });
+ }
 google.maps.event.addDomListener(window, 'load', initialize);
