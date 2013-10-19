1e9875cd429bcedc93f7a1ed6312a35c335de62d
Andrei Petre <p31andrei@gmail.com>
2013-10-19 16:54:37 +0300

    Reindent to 4 spaces. #3

--- map.js
+++ map.js
@@ -1,22 +1,24 @@
 var map;
 function initialize() {
-  var mapOptions = {
-    zoom: 18,
-    center: new google.maps.LatLng(45.7291, 24.7019),
-    mapTypeControlOptions: {
-    mapTypeIds: [google.maps.MapTypeId.TERRAIN, google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE, google.maps.MapTypeId.HYBRID],
-    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-         },
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+    var mapOptions = {
+        zoom: 18,
+        center: new google.maps.LatLng(45.7291, 24.7019),
+        mapTypeControlOptions: {
+             mapTypeIds: [google.maps.MapTypeId.TERRAIN,
+                          google.maps.MapTypeId.ROADMAP,
+                          google.maps.MapTypeId.SATELLITE,
+                          google.maps.MapTypeId.HYBRID],
+             style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        },
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+                              mapOptions);
 }
 if (navigator.geolocation) {
-     navigator.geolocation.getCurrentPosition(function (position) {
-         initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-         map.setCenter(initialLocation);
-     });
- }
+    navigator.geolocation.getCurrentPosition(function (position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(initialLocation);
+    });
+}
 google.maps.event.addDomListener(window, 'load', initialize);
